b79f96484bdbb112adf2e03b43da636009cbf2ef
gouravgit-12 <168168647+gouravgit-12@users.noreply.github.com>
2024-04-26 13:42:43 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,4 +1,21 @@
 def longest_substring(s: str) -> int:
+    longest_substring_length = 0
+    
+    for i in range(len(s)):
+        current_string_set = set()
+        
+        for x in range(i, len(s)):
+            if s[x] in current_string_set:
+                break
+            else:
+                current_string_set.add(s[x])
+        
+        longest_substring_length = max(
+            longest_substring_length,
+            len(current_string_set)
+        )
+    
+    return longest_substring_length
     
     """"
     
